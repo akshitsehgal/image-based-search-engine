a27f3d80bb2b99ba0a9b47ac4709936c3e5c8c51
Akshit Sehgal <84592564+akshitsehgal@users.noreply.github.com>
2022-01-03 17:08:14 +0530

    Add files via upload

--- face_detector_recognition.py
+++ face_detector_recognition.py
@@ -7,7 +7,7 @@ import glob
 
 detector = dlib.get_frontal_face_detector()
 
-def MyRec(rgb,x,y,w,h,v=20,color=(200,0,0),thikness =2):
+def MyRec(rgb,x,y,w,h,v=25,color=(200,0,0),thikness =2):
     """To draw stylish rectangle around the objects"""
     cv2.line(rgb, (x,y),(x+v,y), color, thikness)
     cv2.line(rgb, (x,y),(x,y+v), color, thikness)
@@ -29,12 +29,7 @@ def save(img,name, bbox, width=180,height=227):
 
 
 # Define a function that loads and encodes an image
-def load_and_encode_img(filename):
-    '''
-    Loads in an image from it's directory using 
-    face_recognition.load_image_file method and returns it's 
-    face encodings.
-    '''
+def load_and_save_img(filename,counter,path):
     
     # Load the input image
     frame =cv2.imread(filename)
@@ -42,40 +37,65 @@ def load_and_encode_img(filename):
     faces = detector(image)
     fit =20
     # detect the face
-    for counter,face in enumerate(faces):
+    #if len(faces)>1:
+
+    for counter1,face in enumerate(faces):
         print(counter)
         x1, y1 = face.left(), face.top()
         x2, y2 = face.right(), face.bottom()
         cv2.rectangle(frame,(x1,y1),(x2,y2),(220,255,220),1)
-        MyRec(frame, x1, y1, x2 - x1, y2 - y1, 10, (0,250,0), 3)
+        MyRec(frame, x1, y1, x2 - x1, y2 - y1, 1, (0,250,0), 3)
         #save(image,new_path+str(counter),(x1-fit,y1-fit,x2+fit,y2+fit))
-        new_path = f'images/extracted_images/detector_recog_model{counter}'
+        new_path = f'{path}_{counter}_{counter1}'
         save(frame,new_path,(x1,y1,x2,y2))
         #frame = cv2.resize(frame,(800,800))
         cv2.imshow('img',frame)
         cv2.waitKey(0)
         print("done saving")
-    for img in glob.glob('images/extracted_images' + '/' + '*.jpg'):
-        img = cv2.imread(img)
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        encoded_image = face_recognition.face_encodings(img)[0]
-        print(encoded_image)
 
+        
+
+
+def encode_image(img):
+    img = cv2.imread(img)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    encoded_image = face_recognition.face_encodings(img)[0]
+
+    # print(encoded_image)
     return encoded_image
 
 # Store the encoded vec in a dictionary
 count = 0
+counter = 9
 encoded_vec = {}
 encoded_vec_list = []
 image_vec = {}
 
-for image in glob.glob('images/Train' + '/' + '*.jpg'):
+for image in glob.glob('images/Train' + '/' + '*.*'):
     print(image)
-    count = count + 1
-    img_encoding = load_and_encode_img(image)
-    #encoded_vec[count] = img_encoding
-    image_vec[count] = image
+    #count = count + 1
+    counter= counter + 1
+    load_and_save_img(image,counter,'images/extracted_images/')
+    frame =cv2.imread(image)
+    img =cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    faces = detector(img)
+    print(len(faces))
+    if len(faces)>1:
+        for i in range(len(faces)):
+            print('saved_twice')
+            count = count+1
+            image_vec[count] = image
+    else:
+      count = count + 1
+      image_vec[count] = image
+for img in glob.glob('images/extracted_images' + '/' + '*.*'):
+    print(img)
+    try:
+      img_encoding = encode_image(img)
+    except:
+        continue
     encoded_vec_list.append(img_encoding)
+
 
 
 print(encoded_vec_list)
@@ -83,15 +103,53 @@ print(encoded_vec_list)
 
 # Testing our encodings
 
-encoded_img_test = load_and_encode_img('images\Test\depositphotos_29133839-stock-photo-group-of-girls-chilling-on.jpg')
-
-
-# Compare
-results = face_recognition.compare_faces(encoded_vec_list,encoded_img_test)
-print(results)
-
-for i in range(len(encoded_vec_list)):
-    if results[i] == True:
-        image = cv2.imread(image_vec[i+1])
-        cv2.imshow('Image',image)
+#test_path = load_and_save_img('images\Train/apj.jpg',20)
+test_path = 'images\images_for_extraction/apj(3).jpg0.jpg0.jpg'
+count_test = 0
+counter_test = 9
+encoded_vec_list_test = []
+counter_test= counter_test + 1
+frame_test =cv2.imread(test_path)
+img_test =cv2.cvtColor(frame_test,cv2.COLOR_BGR2RGB)
+faces_test = detector(img_test)
+print(len(faces_test))
+if len(faces_test)>1:
+    results = []
+    load_and_save_img(test_path,counter_test,'images/test_extracted_images/')
+    for img_test in glob.glob('images/test_extracted_images' + '/' + '*.*'):
+        try:
+            img_encoded_test = encode_image(img_test)[0]
+        except:
+            continue
+        results = face_recognition.compare_faces(encoded_vec_list,img_encoded_test,tolerance=0.6)
+        print(results)
+        image_test = cv2.imread(test_path)
+        cv2.imshow('Image',image_test)
         cv2.waitKey(0)
+        for i in range(len(encoded_vec_list)):
+            if results[i] == True:
+                image = cv2.imread(image_vec[i+1])
+                cv2.imshow('Image',image)
+                cv2.waitKey(0)
+
+else:
+    img_encoding_test = encode_image(test_path)
+
+
+
+
+    # Compare
+    results = face_recognition.compare_faces(encoded_vec_list,img_encoding_test,tolerance=0.6)
+    print(results)
+
+    image_test = cv2.imread(test_path)
+    cv2.imshow('Image',image_test)
+    cv2.waitKey(0)
+
+
+
+    for i in range(len(encoded_vec_list)):
+        if results[i] == True:
+            image = cv2.imread(image_vec[i+1])
+            cv2.imshow('Image',image)
+            cv2.waitKey(0)
